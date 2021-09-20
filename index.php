13df149fc2a033365542e6d3b74a830e69a2c57c
alexsoyes <contact.alexsoyes@gmail.com>
2021-09-20 15:02:06 +0200

    Exo 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$animes = [
+    [
+        'name' => 'MHA',
+        'description' => '',
+        'seasons' => 5,
+        'tags' => ['Action', 'Shonen'],
+        'favorited' => true,
+    ],
+    [
+        'name' => 'Naruto',
+        'description' => '',
+        'seasons' => 1000,
+        'tags' => ['Action', 'Shonen'],
+        'favorited' => false,
+    ],
+];
