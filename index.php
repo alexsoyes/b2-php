aecfaf4200034702183ce541525d886a9ca66227
alexsoyes <contact.alexsoyes@gmail.com>
2021-09-27 15:31:04 +0200

    Exo 5 : Formulaire ajout anime

--- index.php
+++ index.php
@@ -1,12 +1,49 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cours PHP</title>
+    <link rel="stylesheet" href="https://unpkg.com/mvp.css">
+</head>
+
+<body>
+
 <?php
+
+phpinfo();
+
+session_start();
 
 error_reporting(-1);
 ini_set('error_reporting', E_ALL);
 
 require "database.php";
 
-initDatabase();
-prefillData();
+
+if ($_POST && !empty($_POST)) {
+
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $season = $_POST['seasons'];
+    $note = $_POST['note'];
+    $tags = explode(',', $_POST['tags']);
+
+    insert([
+        'name' => $name,
+        'description' => $description,
+        'seasons' => $season,
+        'note' => $note,
+        'tags' => $tags,
+    ]);
+
+    header('Location: /');
+}
+
+//initDatabase();
+//prefillData();
 
 /**
  * @param int $note la note de mon anime
@@ -56,3 +93,31 @@ $animes = getData();
         </tbody>
     </table>
 <?php endif; ?>
+
+<form action="" method="post">
+    <label>Nom
+        <input type="text" name="name" value="JJK">
+    </label>
+
+    <label>Description
+        <input type="text" name="description" value="Extension du territoire">
+    </label>
+
+    <label>Saison
+        <input type="number" name="seasons" readonly value="5">
+    </label>
+
+    <label>Note
+        <input type="number" name="note" required value="10">
+    </label>
+
+    <label>Tags (séparé par des virgules)
+        <input type="text" name="tags" required value="Action,Shonen">
+    </label>
+
+    <button type="submit">Enregistrer</button>
+</form>
+
+
+</body>
+</html>
